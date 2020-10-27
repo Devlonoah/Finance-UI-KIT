f2f61cf9a1f37514c05ccec8588a775545c52bd5
Looper101 <hemkeys@gmail.com>
2020-10-26 18:00:35 -0600

    menu done

--- lib/screens/menu_screen/menu.dart
+++ lib/screens/menu_screen/menu.dart
@@ -1,14 +1,81 @@
+import 'package:finance/constants.dart';
 import 'package:flutter/material.dart';
 
 class Menu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Column(
-      children: <Widget>[
-        Icon(Icons.room),
-        Text('Find ATMs'),
-      ],
-    ));
+      margin: EdgeInsets.symmetric(vertical: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          menuList(
+            context: context,
+            icon: Icons.room,
+            title: 'Find Atm',
+            onTap: () {},
+          ),
+          menuList(
+            context: context,
+            icon: Icons.show_chart,
+            title: 'Statistics',
+            onTap: () {},
+          ),
+          menuList(
+            context: context,
+            icon: Icons.local_car_wash,
+            title: 'Interest Calculator',
+            onTap: () {},
+          ),
+          menuList(
+            context: context,
+            icon: Icons.adjust,
+            title: 'Budget Goal',
+            onTap: () {},
+          ),
+          menuList(
+            context: context,
+            icon: Icons.person_add,
+            title: 'Invites',
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget menuList({
+    Function onTap,
+    IconData icon,
+    String title,
+    BuildContext context,
+  }) {
+    Size size = MediaQuery.of(context).size;
+    return InkWell(
+      splashColor: kBackgroundColor,
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          vertical: size.height * 0.04,
+          horizontal: size.width * 0.03,
+        ),
+        child: Row(
+          children: <Widget>[
+            Icon(
+              icon,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: size.width * 0.05,
+            ),
+            Text(
+              title,
+              style:
+                  TextStyle(color: Colors.white, fontSize: size.width * 0.05),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
